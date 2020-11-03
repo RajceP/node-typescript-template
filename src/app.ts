b5ae35f0d5db432743e51c39b683493d210679d5
Petr Rajtslegr <petr.rajtslegr@gmail.com>
2020-11-03 08:41:33 +0100

    express, cors, morgan, dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,21 @@
-console.log('It works!');
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import morgan from 'morgan';
+
+const { config } = dotenv;
+config();
+
+const app = express();
+const { urlencoded, json } = express;
+
+app.use(cors({ credentials: true, origin: true }));
+app.options('*', cors());
+app.use(morgan('dev'));
+app.use(urlencoded({ extended: true }));
+app.use(json());
+
+const port = process.env.PORT || 3002;
+app.listen(port, () => {
+  console.log(`server listening on port ${port}`);
+});
